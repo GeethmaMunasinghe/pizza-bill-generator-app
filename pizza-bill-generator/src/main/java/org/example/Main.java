7a3a762f3b0743ed95ee27ba171cbc55d2fae047
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-05-03 20:25:45 +0530

    create Pizza class

--- pizza-bill-generator/src/main/java/org/example/Main.java
+++ pizza-bill-generator/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pizza basePizza=new Pizza(true);
+        basePizza.getPizzaPrice();
     }
 }
---
+++ pizza-bill-generator/src/main/java/org/example/Pizza.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Pizza {
+    private int price;
+    private Boolean veg;
+
+    public Pizza(Boolean veg) {
+        this.veg = veg;
+        if (this.veg){
+            this.price=300;
+        }else {
+            this.price=400;
+        }
+    }
+
+
+}
